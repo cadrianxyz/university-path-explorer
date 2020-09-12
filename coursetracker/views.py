e0a52f237d05a7158f42f50fea4c68d36fea3449
Clarence Adrian <clarenceadrian@alumni.ubc.ca>
2020-09-12 16:09:43 -0700

    temp details page working

--- coursetracker/views.py
+++ coursetracker/views.py
@@ -2,14 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 from .models import *
+from oakhacksvancoders.scrapers import ubcexplorer
 # Create your views here.
 
 def home(request):
     return render(request, 'coursetracker/search.html')
 
 def course(request, pk):
-    course = Course.objects.get(id=pk)
-    return render(request, 'coursetracker/course.html')
+    # course = Course.objects.get(id=pk)
+
+    # use the given id to do the lookup
+    courseInfo = ubcexplorer.generateCoursePrereqTree('ELEC 202')
+    # replace above line with the following (if taking too long) courseInfo = {}
+
+    return render(request, 'coursetracker/course.html', { 'courseData': courseInfo })
 
 class ClubChartView(TemplateView):
     template_name = 'coursetracker/chart.html'
